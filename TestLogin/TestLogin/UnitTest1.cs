f27d1b15d891aceef2160f2102265a1df452bf25
ndhung <danghung1354@gmail.com>
2019-04-06 14:20:05 +0700

    a

---
+++ TestLogin/TestLogin/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuanLyQuanCF.DAO;
+using QuanLyQuanCF;
+using System.Data;
+
+namespace TestLogin
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
+
+        private TestContext testContext;
+
+        public TestContext TestContext
+        {
+            get { return testContext; }
+            set { testContext = value; }
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestData.csv", "TestData#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestData.csv"), TestMethod]
+        public void TestLogin()
+        {
+            string username = TestContext.DataRow[0].ToString();
+            string password = TestContext.DataRow[1].ToString();
+            bool expected = Boolean.Parse(TestContext.DataRow[2].ToString());
+
+            fLogin login = new fLogin();
+
+            bool actual = login.Login(username, password);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestQuanLyThucAn.csv", "TestQuanLyThucAn#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestQuanLyThucAn.csv"), TestMethod]
+        public void TestQuanLyThucAn()
+        {
+            //string a = TestContext.DataRow[0].ToString();
+            int id = Int32.Parse(TestContext.DataRow[0].ToString());
+            string name = TestContext.DataRow[1].ToString();
+            float price = float.Parse(TestContext.DataRow[2].ToString());
+            bool expected = Boolean.Parse(TestContext.DataRow[3].ToString());
+
+            FoodDAO login = new FoodDAO();
+
+            bool actual = login.InsertFood(name,id,price);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestXoaThucAn.csv", "TestXoaThucAn#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestXoaThucAn.csv"), TestMethod]
+        public void TestXoaThucAn()
+        {
+            
+            int id = Int32.Parse(TestContext.DataRow[0].ToString());
+            
+            bool expected = Boolean.Parse(TestContext.DataRow[1].ToString());
+
+            FoodDAO login = new FoodDAO();
+
+            bool actual = login.DeleteFood(id);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestUpdateFood.csv", "TestUpdateFood#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestUpdateFood.csv"), TestMethod]
+        public void TestUpdateFood()
+        {
+            //string a = TestContext.DataRow[0].ToString();
+            int idFood = Int32.Parse(TestContext.DataRow[0].ToString());
+            string name = TestContext.DataRow[1].ToString();
+            int id = Int32.Parse(TestContext.DataRow[2].ToString());
+            float price = float.Parse(TestContext.DataRow[3].ToString());
+            bool expected = Boolean.Parse(TestContext.DataRow[4].ToString());
+
+            FoodDAO login = new FoodDAO();
+
+            bool actual = login.UpdateFood(idFood,name, id, price);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
