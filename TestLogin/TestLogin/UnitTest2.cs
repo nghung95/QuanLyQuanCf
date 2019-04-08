ec0920c1f01f4b30e34dd641fe1f3ca3cac9471f
USER-tin <tin@gmail.com>
2019-04-08 18:53:55 +0700

    commit

---
+++ TestLogin/TestLogin/UnitTest2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuanLyQuanCF.DAO;
+using QuanLyQuanCF;
+using System.Data;
+using System.Globalization;
+
+namespace TestLogin
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private TestContext testContext;
+
+        public TestContext TestContext
+        {
+            get { return testContext; }
+            set { testContext = value; }
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestTimKiem.csv", "TestTimKiem#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestTimKiem.csv"), TestMethod]
+        public void TestTimKiem()
+        {
+            string name = TestContext.DataRow[0].ToString();
+            bool expected = Boolean.Parse(TestContext.DataRow[1].ToString());
+
+            fAdmin search = new fAdmin();
+
+            bool actual = false;
+            if (search.SearchFoodByName(name).Count() > 0)
+                actual = true;
+                
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestTimKiemByCategoryId.csv", "TestTimKiemByCategoryId#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestTimKiemByCategoryId.csv"), TestMethod]
+        public void TestTimKiemByCategoryId()
+        {
+            int id = Int32.Parse(TestContext.DataRow[0].ToString());
+            bool expected = Boolean.Parse(TestContext.DataRow[1].ToString());
+
+            FoodDAO search = new FoodDAO();
+
+            bool actual = false;
+            if (search.GetFoodByCategoryID(id).Count() > 0)
+                actual = true;
+
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestGetBillListByDate.csv", "TestGetBillListByDate#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestGetBillListByDate.csv"), TestMethod]
+        public void TestGetBillListByDate()
+        {
+            System.DateTime dtDatetime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            DateTime checkIn = dtDatetime.AddSeconds(Int32.Parse(TestContext.DataRow[0].ToString())).ToLocalTime();
+            DateTime checkOut = dtDatetime.AddSeconds(Int32.Parse(TestContext.DataRow[1].ToString())).ToLocalTime();
+            int count = Int32.Parse(TestContext.DataRow[2].ToString());
+            bool expected = Boolean.Parse(TestContext.DataRow[3].ToString());
+
+            BillDAO bill = new BillDAO();
+
+            bool actual = false;
+            if (bill.GetBillListByDate(checkIn, checkOut).Rows.Count == count)
+                actual = true;
+
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
