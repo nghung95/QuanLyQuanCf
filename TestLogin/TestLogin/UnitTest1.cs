edd87f887b75ab8c7cbf641fbade76d8c84e375a
ndhung <danghung1354@gmail.com>
2019-04-15 22:14:37 +0700

    ...

--- TestLogin/TestLogin/UnitTest1.cs
+++ TestLogin/TestLogin/UnitTest1.cs
@@ -79,5 +79,51 @@ namespace TestLogin
 
             Assert.AreEqual(expected, actual);
         }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestTimKiem.csv", "TestTimKiem#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestTimKiem.csv"), TestMethod]
+        public void TestTimKiem()
+        {
+            string name = TestContext.DataRow[0].ToString();
+            bool expected = Boolean.Parse(TestContext.DataRow[1].ToString());
+
+            fAdmin search = new fAdmin();
+
+            bool actual = false;
+            if (search.SearchFoodByName(name).Count() > 0)
+                actual = true;
+
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestTimKiemByCategoryId.csv", "TestTimKiemByCategoryId#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestTimKiemByCategoryId.csv"), TestMethod]
+        public void TestTimKiemByCategoryId()
+        {
+            int id = Int32.Parse(TestContext.DataRow[0].ToString());
+            bool expected = Boolean.Parse(TestContext.DataRow[1].ToString());
+
+            FoodDAO search = new FoodDAO();
+
+            bool actual = false;
+            if (search.GetFoodByCategoryID(id).Count() > 0)
+                actual = true;
+
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestGetUncheckBillIDByTableID.csv", "TestGetUncheckBillIDByTableID#csv", DataAccessMethod.Sequential), DeploymentItem("TestLogin\\TestGetUncheckBillIDByTableID.csv"), TestMethod]
+        public void TestGetUncheckBillIDByTableID()
+        {
+            int billId = Int32.Parse(TestContext.DataRow[0].ToString());
+            bool expected = Boolean.Parse(TestContext.DataRow[1].ToString());
+
+            BillDAO bill = new BillDAO();
+
+            bool actual = false;
+            if (bill.GetUncheckBillIDByTableID(billId) != -1)
+                actual = true;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
